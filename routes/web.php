a7875e8679cc505202d85c676f31668b58f25d53
unknown <bpintochecya@gmail.com>
2023-11-03 23:39:44 -0500

    solution

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('form',[CompetitorController::class,'index'])->name('form');
 Route::post('select/value',[CompetitorController::class,'search_select_value']);
 Route::post('form/register',[CompetitorController::class,'register_competitor']);
